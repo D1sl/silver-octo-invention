6c19235816f0635c0d3948aad0b6346828c2817d
D1sl <94911312+D1sl@users.noreply.github.com>
2022-03-11 22:48:38 -0600

    show employees

--- index.js
+++ index.js
@@ -2,36 +2,73 @@ const inquirer = require("inquirer");
 const cTable = require("console.table");
 const mysql = require('mysql2');
 const db = require('./db/connection');
+let currentAction = "mainmenu";
+
+function init() {
+    console.clear();
+    console.log(`
+    ==========================
+    EMPLOYEE TRACKER v.0.1
+    ==========================`)
+    menu();
+}
+
 
 function getEmployees() {
+    console.clear();
     const sql = `SELECT * FROM employee`;
     db.query(sql, (err, rows) => {
         if (err) {
             console.log(err);
         }
         console.table(rows);
+        currentAction = "viewEmployees";
     })
 }
 
-function mainMenu() {
-    inquirer
-        .prompt({
-            type: 'list',
-            name: 'main',
-            message: 'Choose an action',
-            choices: ['Show employees', 'Quit']
-        })
-        .then(function(action) {
-            switch (action.main) {
-                case 'Show employees':
-                    menuEmployees();                    
-                    break;
-            }
-        })
+function menu() {
+    switch (currentAction) {
+        case currentAction = "mainmenu":
+            inquirer
+                .prompt({
+                    type: 'list',
+                    name: 'main',
+                    message: 'Choose an action',
+                    choices: ['Show employees', 'Quit']
+                })
+                .then(function (action) {
+                    switch (action.main) {
+                        case 'Show employees':
+                            currentAction = "showEmployees";
+                            menu();
+                            break;
+                    }
+
+                })
+            break;
+
+            case currentAction = "showEmployees":
+            getEmployees();
+            inquirer
+            .prompt({
+                type: 'list',
+                name: 'employeeActions',
+                message: 'Choose an action',
+                choices: ['Edit', 'Back to Main Menu']
+            })
+            .then(function (action) {
+                switch (action.employeeActions) {
+                    case 'Back to Main Menu':
+                        init();
+                }
+            })
+            break;
+
+
+        default:
+            break;
+    }
+
 }
 
-function menuEmployees() {
-    console.log("Show Employees Chosen!")
-}
-
-mainMenu();
+init();
