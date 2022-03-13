943c9484742b8e499040334c2091164361bb8551
D1sl <94911312+D1sl@users.noreply.github.com>
2022-03-13 09:47:02 -0500

    dynamically get roles from the database to feed into the addEmployee function

--- index.js
+++ index.js
@@ -3,11 +3,37 @@ const cTable = require("console.table");
 const mysql = require('mysql2');
 const db = require('./db/connection');
 
-console.clear();
+
+
+// Update everything
+function updateServer() {
+    db.query("SELECT * from roles", function (error, res) {
+        allroles = res.map(role => ({ name: role.title, value: role.id }));
+    });
+
+    db.query("SELECT * from departments", function (error, res) {
+        alldepartments = res.map(dept => ({ name: dept.name, value: dept.id }));
+    });
+
+    db.query("SELECT * from employees", function (error, res) {
+        allemployees = res.map(employee => ({
+            name: `${employee.first_name} ${employee.last_name}`,
+            value: employee.id
+        }));
+    });
+}
+
+db.connect(function(err) {
+    if (err) throw err;
+    updateServer();
+});
+
+
+// console.clear();
 
 function getEmployees() {
-    console.clear();
-    const sql = `SELECT id, first_name, last_name FROM employees`;
+    // console.clear();
+    const sql = `SELECT employees.first_name AS "First Name", employees.last_name AS "Last Name", roles.title AS "Title", roles.salary AS "Salary" FROM employees LEFT JOIN roles ON employees.role_id = roles.id;`;
     db.query(sql, (err, rows) => {
         if (err) {
             console.log(err);
@@ -18,13 +44,41 @@ function getEmployees() {
     })
 }
 
-const addEmployee = () => {
+function getDepartments() {
+    // console.clear();
+    const sql = `SELECT id, department_name FROM departments`;
+    db.query(sql, (err, rows) => {
+        if (err) {
+            console.log(err);
+        }
+        console.table(rows);
+        console.log(`Departments: ${rows.length} \n`);
+        app();
+    })
+}
 
-    
-    console.log("add employee");
+function getRoles() {
+    // console.clear();
+    const sql = `SELECT roles.title, roles.salary FROM roles`;
 
 
-    return inquirer.prompt([
+    // let query = "SELECT roles.title, roles.salary, department.dept_name AS department FROM roles INNER JOIN department ON department.id = roles.department_id;";
+
+
+    db.query(sql, (err, rows) => {
+        if (err) {
+            console.log(err);
+        }
+        console.table(rows);
+        console.log(`Roles: ${rows.length} \n`);
+        app();
+    })
+}
+
+function addEmployee() {
+
+    updateServer();
+    inquirer.prompt([
         {
             type: 'input',
             name: 'firstname',
@@ -51,10 +105,18 @@ const addEmployee = () => {
                 }
             }
         },
+        {
+            type: 'list',
+            name: 'employeeRole',
+            message: 'Enter the employees role',
+            choices: allroles
+        }
     ])
-    .then(function(answer) {
-        console.log(answer);
-    })
+        .then(function (answer) {
+
+            console.log(answer);
+
+        })
 }
 
 function app() {
@@ -63,12 +125,18 @@ function app() {
             type: 'list',
             name: 'main',
             message: 'Choose an action',
-            choices: ['List employees', 'Add an Employee', 'Quit']
+            choices: ['List employees', 'List departments', 'List roles', 'Add an Employee', 'Quit']
         })
         .then(function (action) {
             switch (action.main) {
-                case 'Show employees':
+                case 'List employees':
                     getEmployees();
+                    break;
+                case 'List departments':
+                    getDepartments();
+                    break;
+                case 'List roles':
+                    getRoles();
                     break;
                 case 'Add an Employee':
                     addEmployee();
